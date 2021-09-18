d4c4270274c67690a9bf82a84f1c55d46584f6d9
DevMiguell <buguel2000@gmail.com>
2021-09-17 22:16:20 -0300

    feat: add User Wrapper and percentage lib

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Container, Title } from './styles'
+
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/53983988?s=400&u=74f3c2c08f875bf0aa54d1461c6b697a1d3f4dff&v=4' }}
+            />
+
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Miguel</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
